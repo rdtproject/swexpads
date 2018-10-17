2cdde3c1edc8788836c78b6bc5cc75c8334f46a2
Pet User <pet@email.com>
2018-10-17 19:23:16 +0200

    Initial commit

---
+++ src/main/java/pl/webcache/swexpads/SwexpadsApplication.java
@@ -0,0 +1,12 @@
+package pl.webcache.swexpads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwexpadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwexpadsApplication.class, args);
+	}
+}
